2f2e635bf012b015111100859157c437e1ebd956
Alex Plaknava <alejandro.plancarte@estudiante.uacm.edu.mx>
2014-11-20 11:28:29 -0600

    Delete inactivity.js

--- inactivity.js
+++
@@ -1,8 +0,0 @@
-function killerSession(){                
-                setTimeout(function(){
-
-                    alert("ESTA SESION HA CADUCADO, DEBES INICIAR SESION NUEVAMENTE.");
-                    window.close('home.php');
-                    open('logout.php'); }, 10000);
-            }
-}
